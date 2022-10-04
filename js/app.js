3bb7981c1565937eb9a784dee442a6891049b158
obaidmuneer <obaidmuneer55@gmail.com>
2022-10-04 21:35:33 +0500

    api updated for db

--- js/app.js
+++ js/app.js
@@ -5,35 +5,29 @@ let item = document.querySelector('#item')
 let result = document.querySelector('#result')
 let todo = document.querySelector('#todo')
 
-let todoItems = {}
-let oldItemId;
-let api = 'https://faithful-erin-wig.cyclic.app/'
-// let api = 'http://localhost:8080/'
+let todos = {}
+let todoId;
+// let api = 'https://faithful-erin-wig.cyclic.app/'
+let api = 'http://localhost:8080/'
 
 function addCourse() {
     if (!courseItem.value) {
         return
     }
     localStorage.setItem('course', courseItem.value)
-    axios.post(`${api}courses`, {
-        course: courseItem.value
-    })
-        .then((res) => {
-            todoItems = res.data.data
-            showItems()
-            // console.log(todoItems);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+    renderItems()
+    item.focus()
+
 
 }
 function renderItems(items) {
-    axios.get(`${api}todoItems`)
+    axios.get(`${api}todos/${courseItem.value}`)
         .then((res) => {
-            // console.log(res.data.data);
-            todoItems = res.data.data
-            console.log(todoItems);
+            todos = res.data.data
+            // todos = todos.filter((item, index) => {
+            //     return item.course === courseItem.value
+            // })
+            // console.log(todos);
             showItems()
         })
         .catch((err) => {
@@ -51,49 +45,27 @@ function addItem(index) {
     if (!item.value) {
         return
     }
-    const todoItem = {
-        text: item.value,
-        id: Date.now()
-    }
-    // console.log(index);
-    index > -1 ? updateItem(index) : postItem(courseItem.value, todoItem)
+    index > -1 ? updateItem(index) : postItem(courseItem.value, item.value)
     item.value = ''
 }
 
-function fileUpload() {
-    let formData = new FormData();
-    formData.append("uploadedFile", imagefile.files[0]);
-    axios.post(`${api}${courseItem.value}/upload`, formData, {
-        headers: {
-            'Content-Type': 'multipart/form-data'
-        }
+function postItem(course, text) {
+    axios.post(`${api}todo`, {
+        course,
+        text
     })
         .then((res) => {
-            console.log(res.data.data);
             renderItems()
-            // document.querySelector('#img').src = res.data.data
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-}
-
-function postItem(course, todoItem) {
-    axios.post(`${api}todoItem`, {
-        todoItem,
-        course
-    })
-        .then((res) => {
-            todoItems = res.data.data
-            showItems()
+            // console.log(res.data);
+            // todos = res.data
+            // showItems()
         })
 }
 
 
 function showItems() {
     result.innerHTML = ''
-
-    todoItems[courseItem.value].items.reverse().map((item, index) => {
+    todos.reverse().map((item, index) => {
         let edit = item.text ? `<a href="#" onclick="editItem(${index})" class="text-info" data-mdb-toggle="tooltip" title="Edit todo"><i
         class="fas fa-pencil-alt me-3"></i></a>` : ''
         let dlt = `<a href="#" onclick="dltItem(${index})" class="text-danger" data-mdb-toggle="tooltip" title="Delete todo"><i
@@ -110,9 +82,9 @@ function showItems() {
                 <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
             </div>
           </div><br>`
-            
+
         }
-        result.innerHTML += `<li data-key="${item.id}"
+        result.innerHTML += `<li data-key="${item._id}"
                 class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
                 ${itemdiv}
                 <div class="d-flex flex-row justify-content-end mb-1">
@@ -142,12 +114,12 @@ function formSetting(index) {
 }
 
 function updateItem() {
-    axios.put(`${api}/todoItem/${courseItem.value}/${oldItemId}`, {
+    axios.put(`${api}todo/${todoId}`, {
         text: item.value
     })
         .then((res) => {
-            todoItems = res.data.data
-            showItems()
+            // console.log(res.data);
+            renderItems()
         })
         .catch((err) => {
             console.log(err.response.data.msg);
@@ -156,18 +128,18 @@ function updateItem() {
 }
 
 function editItem(index) {
-    item.value = todoItems[courseItem.value].items[index].text
-    oldItemId = todoItems[courseItem.value].items[index].id
-    // console.log(oldItemId);
+    item.value = todos[index].text
+    todoId = todos[index]._id
     item.focus()
     formSetting(index)
 }
 
 function dltItem(index) {
-    axios.delete(`${api}todoItem/${courseItem.value}/${todoItems[courseItem.value].items[index].id}`)
+    todoId = todos[index]._id
+    axios.delete(`${api}todo/${todoId}`)
         .then((res) => {
-            todoItems = res.data.data
-            showItems()
+            console.log(res.data);
+            renderItems()
         })
         .catch((err) => {
             console.log(err.message);
@@ -175,7 +147,7 @@ function dltItem(index) {
 }
 
 function dltAll() {
-    axios.delete(`${api}/${courseItem.value}/`)
+    axios.delete(`${api}todos/${courseItem.value}`)
         .then((res) => {
             console.log(res.data);
             result.innerHTML = ''
